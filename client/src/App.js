da4c0fd576c6cc4e15f9bf26ced54aa61352ef36
ahmed <ahmedthegicoder@gmail.com>
2020-04-07 13:34:16 +0200

    finished react components and graphql client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import logo from './imgs/spacex.jpeg';
+import Launches from './components/Launches';
+import Launch from './components/Launch';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
 
 function App() {
   return (
-    <div className="App">
-      <img src={logo} alt="spacex" style={{width: 300, display: 'block', margin: 'auto'}} />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <div className="App">
+            <img src={logo} alt="spacex" style={{ width: 300, display: 'block', margin: 'auto' }} />
+            <Route exact path="/" component={Launches} />
+            <Route exact path="/launch/:flight_number" component={Launch} />
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
